9b7c7eca5d9b4068ece5e2bd96b54220533d3eb9
willberttengker17 <100185080+willberttengker17@users.noreply.github.com>
2023-05-29 13:17:58 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
--- server.js
+++ server.js
@@ -17,8 +17,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
 });
